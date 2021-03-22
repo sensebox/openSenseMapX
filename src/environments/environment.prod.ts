05856fc3668937f80ef222a024b9cbf927dec08b
Umut Tas <u.tas@reedu.de>
2021-03-22 09:46:25 +0100

    sensor wiki url hardcode

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: true,
   api_url: process.env.API_URL,
-  sensor_wiki_url: process.env.SENSOR_WIKI_URL,
+  sensor_wiki_url: "https://sensor-wiki.opensensemap.org",
   mapbox_token: process.env.MAPBOX_TOKEN,
   locationiq_token: process.env.LOCATIONIQ_TOKEN
 };
