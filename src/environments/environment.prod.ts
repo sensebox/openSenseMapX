c2e4651952dc445714acb23bba8ad63ca140e55e
Umut Tas <u.tas@reedu.de>
2021-03-22 09:37:14 +0100

    add variable for sensor wiki

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,7 @@
 export const environment = {
   production: true,
   api_url: process.env.API_URL,
+  sensor_wiki_url: process.env.SENSOR_WIKI_URL,
   mapbox_token: process.env.MAPBOX_TOKEN,
   locationiq_token: process.env.LOCATIONIQ_TOKEN
 };
