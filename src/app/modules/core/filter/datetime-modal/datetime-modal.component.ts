2810e0fe8ee5de5677f72562057ce0561196f1b3
Umut Tas <u.tas@reedu.de>
2021-06-22 10:55:53 +0200

    map datepicker to bulma datepicker

--- src/app/modules/core/filter/datetime-modal/datetime-modal.component.ts
+++ src/app/modules/core/filter/datetime-modal/datetime-modal.component.ts
@@ -2,6 +2,7 @@ import { Input, Output } from '@angular/core';
 import { Component, EventEmitter, OnInit } from '@angular/core';
 import { TranslateService } from '@ngx-translate/core';
 
+import bulmaCalendar from 'node_modules/bulma-calendar/dist/js/bulma-calendar.min.js';
 
 @Component({
   selector: 'osem-datetime-modal',
@@ -16,30 +17,36 @@ export class DatetimeModalComponent implements OnInit {
   @Output() changedDateStamp = new EventEmitter();
   @Output() closed = new EventEmitter();
   @Input() active = 'live';
+
+  dateRangeStart;
+  dateRangeEnd;
   @Input() set dateRange(value){
     if(value){
       this.dateRangeStart = value[0];
       this.dateRangeEnd = value[1];
     }
   };
+  dateStampValue;
+  @Input() set dateStamp(value){
+    if(value){
+      this.dateStampValue = value;
+    }
+  };
 
   startAt = new Date('2020-03-27T10:00:00.000Z');
   minDate = new Date('2020-03-27T10:00:00.000Z');
-  maxDate = new Date('2020-03-27T14:00:00.000Z');
-
-  dateRangeStart;
-  dateRangeEnd;
-  dateStamp;
+  maxDate = new Date(); // TODO: Round this to nearest 5minutes ?
+ 
 
 
   constructor(public translateService: TranslateService) { }
 
-  ngOnInit() {
-    
+  ngOnInit() {        
   }
 
   changeActive(active){
     this.changedActive.emit(active);
+    this.initCalendars();
   }
 
   close(){
@@ -61,6 +68,52 @@ export class DatetimeModalComponent implements OnInit {
   updateDateStamp(date){
     this.changedDateStamp.emit(date);
     this.closed.emit();
+  }
+
+  initCalendars(){
+    let that = this;
+    setTimeout(function(){
+      const timeStampOptions = {
+        lang: that.translateService.currentLang === 'de-DE' ? 'de' : 'en',
+        minuteSteps: 10,
+        startTime: that.dateStampValue ? that.dateStampValue : that.startAt,
+        startDate: that.dateStampValue ? that.dateStampValue : that.startAt,
+        maxDate: that.maxDate,
+        color: '#4EAF47'
+      }
+
+      const timeStampCalendar = bulmaCalendar.attach('#timestamp', timeStampOptions);
+      const timeRangeOptions = {
+        lang: that.translateService.currentLang === 'de-DE' ? 'de' : 'en',
+        minuteSteps: 10,
+        startDate: that.dateRangeStart ? that.dateRangeStart : that.minDate,
+        startTime: that.dateRangeStart ? that.dateRangeStart : that.minDate,
+        endDate: that.dateRangeEnd ? that.dateRangeEnd : that.minDate,
+        endTime: that.dateRangeEnd ? that.dateRangeEnd : that.minDate,
+        maxDate: that.maxDate,
+        color: '#4EAF47'
+      }
+      const timeRangeCalendar = bulmaCalendar.attach('#timerange', timeRangeOptions);
+  
+      // To access to bulmaCalendar instance of an element
+      const element:any = document.querySelector('#timestamp');
+      if (element) {
+        // bulmaCalendar instance is available as element.bulmaCalendar
+        element.bulmaCalendar.on('select', datepicker => {
+          that.dateStampValue = new Date(datepicker.data.value());
+        });
+      }
+      const elementRange:any = document.querySelector('#timerange');
+      if (elementRange) {
+        // bulmaCalendar instance is available as element.bulmaCalendar
+        elementRange.bulmaCalendar.on('select', datepicker => {
+          that.dateRangeStart = datepicker.data.startDate;
+          that.dateRangeEnd = datepicker.data.endDate;
+        });
+      }
+
+    },5)
+
   }
 
 }
