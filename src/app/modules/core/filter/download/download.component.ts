78495dd4a7aaecca2501742dc112a42bc2c8bd48
Matthias Pfeil <matthias.pfeil@wwu.de>
2021-08-16 11:57:35 +0200

    Add basic ngModel functionality

--- src/app/modules/core/filter/download/download.component.ts
+++ src/app/modules/core/filter/download/download.component.ts
@@ -1,15 +1,35 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'osem-download',
   templateUrl: './download.component.html',
   styleUrls: ['./download.component.scss']
 })
-export class DownloadComponent implements OnInit {
+export class DownloadComponent {
 
-  constructor() { }
+  downloadDetails = {
+    window: 'raw',
+    operation: 'arithmeticMean',
+    boxId: true,
+    lat: true,
+    lng: true,
+    height: false,
+    boxName: true,
+    exposure: false,
+    unit: true,
+    value: true,
+    createdAt: true,
+    phenomenon: false,
+    sensorId: false,
+    sensorType: false,
+    format: 'csv'
+  };
 
-  ngOnInit() {
+  windows = ['raw', '10m', '1h', '1d'];
+  operations = ['arithmeticMean', 'harmonicMean', 'geometricMean', 'min', 'max', 'mode', 'median', 'variance', 'rootMeanSquare', 'standardDeviation', 'sum'];
+
+  download() {
+    window.alert("ALERT");
   }
 
 }
