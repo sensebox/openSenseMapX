1377592648b6ba52999acff1e3055e93664d613f
Umut Tas <u.tas@reedu.de>
2021-09-15 15:02:38 +0200

    profile boxes

--- src/app/modules/profile/profile-boxes/profile-boxes.component.ts
+++ src/app/modules/profile/profile-boxes/profile-boxes.component.ts
@@ -4,12 +4,19 @@ import { UiService } from 'src/app/models/ui/state/ui.service';
 @Component({
   selector: 'osem-profile-boxes',
   templateUrl: './profile-boxes.component.html',
-  styleUrls: ['./profile-boxes.component.scss'],
-  changeDetection: ChangeDetectionStrategy.OnPush
+  styleUrls: ['./profile-boxes.component.scss']
 })
 export class ProfileBoxesComponent implements OnInit {
 
-  @Input() boxes;
+  @Input() set boxes(boxes) {
+    if(boxes){
+      this.sorted_boxes = boxes;
+    }
+  };
+
+  sorting = 'desc';
+  listStyle = 'boxes';
+  sorted_boxes = [];
 
   constructor(private uiService: UiService) { }
 
@@ -18,6 +25,19 @@ export class ProfileBoxesComponent implements OnInit {
 
   showMyBoxesOnMap() {
     this.uiService.setFilterIds(this.boxes.map(box => box._id));
+  }
+
+  sort(direction){
+    this.sorting = direction;
+    if(direction === 'asc'){
+      this.sorted_boxes = this.sorted_boxes.sort((a,b) => new Date(a.createdAt).getMilliseconds() - new Date(b.createdAt).getMilliseconds())
+    } else {
+      this.sorted_boxes = this.sorted_boxes.sort((a,b) => new Date(b.createdAt).getMilliseconds() - new Date(a.createdAt).getMilliseconds())
+    }
+  }
+
+  setListStyle(style){
+    this.listStyle = style;
   }
 
 }
