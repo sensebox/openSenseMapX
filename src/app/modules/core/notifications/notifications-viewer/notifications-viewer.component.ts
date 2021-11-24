3225c69dadea606d23a5ff6f7433f115625f5d89
PaulaScharf <paulascharf@gmx.de>
2021-11-24 10:23:02 +0100

    adjust look of notifications

--- src/app/modules/core/notifications/notifications-viewer/notifications-viewer.component.ts
+++ src/app/modules/core/notifications/notifications-viewer/notifications-viewer.component.ts
@@ -1,6 +1,7 @@
 import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
 import { NotificationsQuery } from 'src/app/models/notifications/state/notifications.query';
 import { NotificationsService } from 'src/app/models/notifications/state/notifications.service';
+import { Router, ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'osem-notifications-viewer',
@@ -12,9 +13,16 @@ export class NotificationsViewerComponent implements OnInit {
   @Input() notifications;
   @Input() areNotificationsLoaded;
 
-  constructor(private notificationsService: NotificationsService, private notificationsQuery: NotificationsQuery) { }
+  constructor(private router: Router, private activatedRoute: ActivatedRoute, private notificationsService: NotificationsService, private notificationsQuery: NotificationsQuery) { }
 
   async ngOnInit() {
+  }
+  
+  boxDetails(id) {
+    this.router.navigate(['/explore/' + id], {
+      relativeTo: this.activatedRoute,
+      queryParamsHandling: 'merge'
+    }); 
   }
 
 }
