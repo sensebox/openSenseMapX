f1043f3bf877736126f9524799602e932c755c6e
Paula Scharf <48286621+PaulaScharf@users.noreply.github.com>
2022-01-26 12:07:51 +0100

    catch errors when initializing notification rules

--- src/app/models/notifications/state/notifications.service.ts
+++ src/app/models/notifications/state/notifications.service.ts
@@ -30,38 +30,44 @@ export class NotificationsService {
       this.notificationsStore.set({});
       let notifications = [];
       for (let i = 0; i < res.data.length; i++) {
-        let notificationRule = res.data[i];
-        let box = await this.getBox(notificationRule.box, headers);
-        for (let j = 0; j < notificationRule.notifications.length; j++) {
-          let notification = notificationRule.notifications[j];
-          let sensors = [];
-          for ( let i = 0; i < notificationRule.sensors.length; i++) {
-            // @ts-ignore
-            sensors.push(box.sensors.find(sensor => sensor._id == notificationRule.sensors[i]))
+        try {
+          let notificationRule = res.data[i];
+          let box = await this.getBox(notificationRule.box, headers);
+          for (let j = 0; j < notificationRule.notifications.length; j++) {
+            let notification = notificationRule.notifications[j];
+            let sensors = [];
+            for ( let i = 0; i < notificationRule.sensors.length; i++) {
+              // @ts-ignore
+              sensors.push(box.sensors.find(sensor => sensor._id == notificationRule.sensors[i]))
+            }
+            notification = {
+              ...notification,
+              timeText: moment(notification.notificationTime).format("DD.MM.YYYY, HH:mm"),
+              type: "threshold",
+              activationOperator: notificationRule.activationOperator,
+              activationThreshold: notificationRule.activationThreshold,
+              ruleName: notificationRule.name,
+              box: box,
+              // @ts-ignore
+              sensors: sensors
+            }
+            notifications.push(notification)
           }
-          notification = {
-            ...notification,
-            timeText: moment(notification.notificationTime).format("DD.MM.YYYY, HH:mm"),
-            type: "threshold",
-            activationOperator: notificationRule.activationOperator,
-            activationThreshold: notificationRule.activationThreshold,
-            ruleName: notificationRule.name,
-            box: box,
+          res.data[i] = {
+            ...notificationRule,
             // @ts-ignore
-            sensors: sensors
+            boxName: box.name,
+            // @ts-ignore
+            boxExposure: box.exposure,
+            // @ts-ignore //TODO: a notificationRule could theoretically have more than one sensor, but Im not sure if we care about that...
+            sensorName: box.sensors.find(sensor => sensor._id == notificationRule.sensors[0]).title,
+            // @ts-ignore
+            boxDate: box.updatedAt,
           }
-          notifications.push(notification)
-        }
-        res.data[i] = {
-          ...notificationRule,
-          // @ts-ignore
-          boxName: box.name,
-          // @ts-ignore
-          boxExposure: box.exposure,
-          // @ts-ignore //TODO: a notificationRule could theoretically have more than one sensor, but Im not sure if we care about that...
-          sensorName: box.sensors.find(sensor => sensor._id == notificationRule.sensors[0]).title,
-          // @ts-ignore
-          boxDate: box.updatedAt,
+        } catch(e) {
+          console.log(e);
+          res.data.splice(i, 1);
+          i--;
         }
       }
       notifications.sort((a,b) => b.notificationTime.localeCompare(a.notificationTime));
@@ -163,6 +169,9 @@ export class NotificationsService {
     return new Promise ((resolve, reject) => {
       this.http.get(this.AUTH_API_URL + '/boxes/' + id, {headers: headers}).subscribe((res:any) => {
         resolve(res)
+      },
+      (err:any) => {
+        reject(err)
       });
     })
   }
