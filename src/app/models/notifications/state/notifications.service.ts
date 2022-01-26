6068860145297225fca8e673619d02f977cddaec
ceciliajrs <cecilia.jrs@hotmail.com>
2022-01-26 12:12:40 +0100

    user can see already followed thresholds. error message appers.

--- src/app/models/notifications/state/notifications.service.ts
+++ src/app/models/notifications/state/notifications.service.ts
@@ -1,24 +1,26 @@
-import { Injectable } from '@angular/core';
+import { Injectable, EventEmitter, ChangeDetectorRef } from '@angular/core';
 import { ID } from '@datorama/akita';
 import { NotificationsStore } from './notifications.store';
 import { NotificationsQuery } from './notifications.query';
-import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { HttpClient, HttpErrorResponse, HttpHeaders } from '@angular/common/http';
 import { environment } from '../../../../environments/environment';
 import { Router } from '@angular/router';
 import { Observable } from 'rxjs/internal/Observable';
+import { catchError } from 'rxjs/operators';
 
 @Injectable({ providedIn: 'root' })
 export class NotificationsService {
 
   AUTH_API_URL = environment.api_url;
+  public postError: EventEmitter<HttpErrorResponse> = new EventEmitter<HttpErrorResponse>();
+  public messageToUser: string;
 
   constructor(
-    private notificationsStore: NotificationsStore, 
-    private notificationsQuery: NotificationsQuery, 
+    private notificationsStore: NotificationsStore,
+    private notificationsQuery: NotificationsQuery,
     private router: Router,
-    private http: HttpClient) {
-
-  }
+    private http: HttpClient
+    ) { }
 
   async getNotificationRules(){
     let headers = new HttpHeaders();
@@ -74,23 +76,29 @@ export class NotificationsService {
   }
 
   createNotificationRule(params, boxName, sensorTitle) {
-    let headers = new HttpHeaders();
-    headers = headers.append('Authorization', 'Bearer '+window.localStorage.getItem('sb_accesstoken'));
-    this.http.post(`${environment.api_url}/notification/notificationRule`, params, {headers: headers}).subscribe((res:any) => {
-      var d = new Date();
-      let newNotification = {
-        type: "notification-rule",
-        boxName: boxName,
-        boxId: params.box,
-        sensorTitle: sensorTitle,
-        timeText: d.getDate() + "." + (d.getMonth()+1) + "." + (String(d.getFullYear()).slice(2,4)) + ", " + d.getHours() + ":" + d.getMinutes()
-      };
-      this.notificationsStore.update(state => ({
-        ...state,
-        notifications: (typeof state.notifications != "undefined") ? [newNotification].concat(state.notifications) : [newNotification]
-      }));
-      this.setNewNotification(newNotification)
-    });
+      let headers = new HttpHeaders();
+      headers = headers.append('Authorization', 'Bearer '+window.localStorage.getItem('sb_accesstoken'));
+      this.http.post(`${environment.api_url}/notification/notificationRule`, params, { headers: headers })
+        .pipe(catchError(err => {
+          this.messageToUser = "Rule already exists.";
+          throw 'An error occurred: ' + err;
+        }))
+        .subscribe((res: any) => {
+          this.messageToUser = "ok";
+          var d = new Date();
+          let newNotification = {
+            type: "notification-rule",
+            boxName: boxName,
+            boxId: params.box,
+            sensorTitle: sensorTitle,
+            timeText: d.getDate() + "." + (d.getMonth() + 1) + "." + (String(d.getFullYear()).slice(2, 4)) + ", " + d.getHours() + ":" + d.getMinutes()
+          };
+          this.notificationsStore.update(state => ({
+            ...state,
+            notifications: (typeof state.notifications != "undefined") ? [newNotification].concat(state.notifications) : [newNotification]
+          }));
+          this.setNewNotification(newNotification);
+        })
   }
 
   setNewNotification(newNotification) {
