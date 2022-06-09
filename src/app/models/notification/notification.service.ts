353625c3796d9bb25771b814fd7690de1f5862af
fbruc03 <65135023+fbruc03@users.noreply.github.com>
2022-06-09 13:03:21 +0200

    fetch notifications on login

--- src/app/models/notification/notification.service.ts
+++ src/app/models/notification/notification.service.ts
@@ -1,16 +1,18 @@
 import { Injectable } from "@angular/core";
-import { NotificationStore } from "./Notification.store";
 import { Notification } from "./notification.model";
 import { HttpClient, HttpHeaders } from "@angular/common/http";
 import { BehaviorSubject } from "rxjs/internal/BehaviorSubject";
+import { SessionQuery } from "../session/state/session.query";
 
 @Injectable({ providedIn: "root" })
 export class NotificationService {
-  constructor(
-    private NotificationStore: NotificationStore,
-    private http: HttpClient
-  ) {
+  constructor(private sessionQuery: SessionQuery, private http: HttpClient) {
     this.fetch();
+    this.sessionQuery.isLoggedIn$.subscribe((isLoggedIn) => {
+      if (isLoggedIn) {
+        this.fetch();
+      }
+    });
   }
 
   notifications: BehaviorSubject<Notification[]> = new BehaviorSubject<
