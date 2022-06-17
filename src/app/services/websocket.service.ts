9a5e6c5ca933416289f6ace51581a1dacf1a8154
fbruc03 <65135023+fbruc03@users.noreply.github.com>
2022-06-17 16:50:57 +0200

    connect websocket only if logged in

--- src/app/services/websocket.service.ts
+++ src/app/services/websocket.service.ts
@@ -16,23 +16,22 @@ export class WebsocketService {
     private sessionQuery: SessionQuery,
     private notificationService: NotificationService
   ) {
-    if (this.sessionQuery.user$) {
-      this.socket = io(environment.websocket_url, {
-        auth: { auth_token: window.localStorage.getItem("sb_accesstoken") },
-      });
-      this.sessionQuery.user$.subscribe((user) => {
+    this.sessionQuery.user$.subscribe((user) => {
+      if (user) {
         this.user$ = user;
-      });
-    }
+        this.socket = io(environment.websocket_url, {
+          auth: { auth_token: window.localStorage.getItem("sb_accesstoken") },
+        });
 
-    this.socket.on("connect", () => {
-      console.log("connected");
-      this.socket.emit("join room", this.user$.email);
-    });
+        this.socket.on("connect", () => {
+          console.log("connected");
+          this.socket.emit("join room", this.user$.email);
+        });
 
-    this.socket.on("notification:create", (notification) => {
-      console.log(notification);
-      this.notificationService.add(notification);
+        this.socket.on("notification:create", (notification) => {
+          this.notificationService.add(notification);
+        });
+      }
     });
   }
 }
